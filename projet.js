e235368f8890d8e78afe2d120fa1f2b2f5fdb469
hamzabenze <75089768+hamzabenze@users.noreply.github.com>
2020-12-17 22:15:44 +0100

    Add files via upload

---
+++ projet.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$("button[name=mok]").click(function(){
+$(".mok").slideToggle();
+});
+});
+
+ $(document).ready(function(){
+$("button[name=bak]").click(function(){
+$(".bak").slideToggle();
+});
+});
+
